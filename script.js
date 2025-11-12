0cb7c87bcbabbe95c1e5b22e06653b4094e66034
Jakub Kołdarz <jakub213x@gmail.com>
2025-11-12 09:17:33 +0100

    Com3_2.3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const tbody = document.querySelector("tbody");
     const token = `UhPZSvlDOVgpnKRuRZwAJETGaUhCiRGO`;
-    const url = `https://cors-anywhere.herokuapp.com/https://www.ncei.noaa.gov/cdo-web/api/v2/locationcategories`;
+    const url = `https://cors-anywhere.herokuapp.com/https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=2010-05-01&enddate=2010-05-01`;
 
     fetch(url, { headers: { token } })
         .then((response) => response.json())
@@ -12,13 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
                 const row = document.createElement("tr");
 
                 const idCell = document.createElement("td");
-                idCell.textContent = station.id;
+                idCell.textContent = station.datatype;
 
                 const nameCell = document.createElement("td");
-                nameCell.textContent = station.name;
+                nameCell.textContent = station.station;
+
+                const dateCell = document.createElement("td");
+                dateCell.textContent = new Date(station.date).toLocaleDateString();
 
                 row.appendChild(idCell);
                 row.appendChild(nameCell);
+                row.appendChild(dateCell);
 
                 tbody.appendChild(row);
             });
